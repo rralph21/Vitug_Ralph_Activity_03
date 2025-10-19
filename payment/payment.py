c7dc66e1722b85d87fecd3d089f799f503705ea0
Ralph.Vitug <rralph@rrc.ca>
2025-10-19 18:02:54 -0500

    feat: PaymentProcess imp:pay_bill

--- payment/payment.py
+++ payment/payment.py
@@ -1,7 +1,24 @@
 """This module defines the Payment class."""
 
-__author__ = ""
-__version__ = ""
+__author__ = "Ralph Vitug"
+__version__ = "1.0.0"
 
 from billing_account.billing_account import BillingAccount
 from payee.payee import Payee
+from patterns.strategy.payment_strategy import PaymentStrategy
+
+class PaymentProcess:
+    """
+    PaymentProcess uses a strategy to execute payments
+    without knowing details of any other strategy.
+    """
+
+    def __init__(self, strategy: PaymentStrategy) -> None:
+        self._strategy = strategy
+
+    def set_strategy(self, strategy: PaymentStrategy) -> None:
+        self._strategy = strategy
+
+    def pay_bill(self, account: BillingAccount, payee: Payee,
+                amount: float) -> str:
+        return self._strategy.process_payment(account, payee, amount)
